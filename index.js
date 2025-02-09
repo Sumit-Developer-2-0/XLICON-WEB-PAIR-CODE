4e90e76f2f04527dae4ebdcd322017c9ee44512f
Sumit-Developer-2-0 <sumitdeveloper2.0@gmail.com>
2025-02-09 10:23:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path'); // Import the path module
 const bodyParser = require("body-parser");
+const helmet = require('helmet');
+// const cors = require('cors'); // If you need CORS
 const PORT = process.env.PORT || 8000;
+
 let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Middleware
+app.use(helmet()); // Add security headers
+// app.use(cors());  // Enable CORS if needed
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`YoutTube: S4 Salman YT\nTelegram: ahmmitech\nGitHub: @salmanytofficial\nInstsgram: ahmmikun\n\nServer running on http://localhost:` + PORT)
-})
+app.use('/code', code);
 
-module.exports = app
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Routes
+app.get('/pair', (req, res) => {
+    res.sendFile('pair.html', { root: path.join(__dirname, 'public') });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('main.html', { root: path.join(__dirname, 'public') });
+});
+
+// Error handling middleware (must be defined after all routes)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`YoutTube: S4 Salman YT\nTelegram: ahmmitech\nGitHub: @salmanytofficial\nInstsgram: ahmmikun\n\nServer running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
